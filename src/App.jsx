866633d865eeafe3715a67727ccb4030a1ebb702
HassanMansoor <hassanmansoor518@gmail.com>
2026-09-09 19:41:06 +0500

    Update repository

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
           width="200"
           style={{
             filter: isOn
-              ? "drop-shadow(0 0 35px rgba(89, 0, 244, 1))"
+              ? "drop-shadow(0 0 35px rgba(255, 0, 0, 1))"
               : "none",
           }}
         />
